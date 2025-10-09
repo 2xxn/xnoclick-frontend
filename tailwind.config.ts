fde1d41a8ce6a03315c4345eedab379f6ef7b792
Jakub <75627864+2xxn@users.noreply.github.com>
2025-10-10 00:36:23 +0200

    fix: finally fix theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,24 +11,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        dark: {
-          "primary": "#6366f1",
-          "secondary": "#8b5cf6", 
-          "accent": "#06b6d4",
-          "neutral": "#2a323c",
-          "base-100": "#1d232a",
-          "base-200": "#191e24",
-          "base-300": "#15191e",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-      },
-      "light"
-    ],
+    themes: true,
     darkTheme: "dark",
   },
 };
